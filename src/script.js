da6cb403c5a3de64a6b8a1a71a183f36a193189e
themightyvicki1 <victoria.greer861@gmail.com>
2023-10-31 18:20:49 -0700

    updated time data

--- src/script.js
+++ src/script.js
@@ -12,38 +12,47 @@ function selectedDropdown(event) {
 let nameSelect = document.querySelector("#selector");
 nameSelect.addEventListener("change", selectedDropdown);
 
-// target first city's date, selecting the element then class in element
-let cityOneElement = document.querySelector("#city-one");
-let cityOneDateElement = cityOneElement.querySelector(".date");
-// target city one's time
-let cityOneTimeElement = cityOneElement.querySelector(".time");
-// time for first city
-let cityOneTime = moment().tz("America/Los_Angeles");
-cityOneDateElement.innerHTML = cityOneTime.format("MMMM D, YYYY");
-cityOneTimeElement.innerHTML = `${cityOneTime.format(
-  "h:mm:ss"
-)} <small id = "ampm">${cityOneTime.format("A")}</small>`;
+// use set interval function to keep time updating
+// this code was made not in its own function so in setInterval use a function without a name
+// update every 1 second (1000)
+setInterval(function () {
+  // target first city's date, selecting the element then class in element
+  let cityOneElement = document.querySelector("#city-one");
+  let cityOneDateElement = cityOneElement.querySelector(".date");
+  // target city one's time
+  let cityOneTimeElement = cityOneElement.querySelector(".time");
+  // time for first city
+  let cityOneTime = moment().tz("America/Los_Angeles");
+  cityOneDateElement.innerHTML = cityOneTime.format("MMMM Do, YYYY");
+  cityOneTimeElement.innerHTML = `${cityOneTime.format(
+    "h:mm:ss"
+  )} <small id = "ampm">${cityOneTime.format("A")}</small>`;
+}, 1000);
 
-// target second city's date, selecting the element then class in element
-let cityTwoElement = document.querySelector("#city-two");
-let cityTwoDateElement = cityTwoElement.querySelector(".date");
-// target city two's time
-let cityTwoTimeElement = cityTwoElement.querySelector(".time");
-// time for second city
-let cityTwoTime = moment().tz("America/Chicago");
-cityTwoDateElement.innerHTML = cityTwoTime.format("MMMM D, YYYY");
-cityTwoTimeElement.innerHTML = `${cityTwoTime.format(
-  "h:mm:ss"
-)} <small id = "ampm">${cityTwoTime.format("A")}</small>`;
+setInterval(function () {
+  // target second city's date, selecting the element then class in element
+  let cityTwoElement = document.querySelector("#city-two");
+  let cityTwoDateElement = cityTwoElement.querySelector(".date");
+  // target city two's time
+  let cityTwoTimeElement = cityTwoElement.querySelector(".time");
+  // time for second city
+  let cityTwoTime = moment().tz("America/Chicago");
+  cityTwoDateElement.innerHTML = cityTwoTime.format("MMMM Do, YYYY");
+  cityTwoTimeElement.innerHTML = `${cityTwoTime.format(
+    "h:mm:ss"
+  )} <small id = "ampm">${cityTwoTime.format("A")}</small>`;
+}, 1000);
 
-// target third city's date, selecting the element then class in element
-let cityThreeElement = document.querySelector("#city-three");
-let cityThreeDateElement = cityThreeElement.querySelector(".date");
-// target city two's time
-let cityThreeTimeElement = cityThreeElement.querySelector(".time");
-// time for second city
-let cityThreeTime = moment().tz("Europe/Gibraltar");
-cityThreeDateElement.innerHTML = cityThreeTime.format("MMMM D, YYYY");
-cityThreeTimeElement.innerHTML = `${cityThreeTime.format(
-  "h:mm:ss"
-)} <small id = "ampm">${cityThreeTime.format("A")}</small>`;
+setInterval(function () {
+  // target third city's date, selecting the element then class in element
+  let cityThreeElement = document.querySelector("#city-three");
+  let cityThreeDateElement = cityThreeElement.querySelector(".date");
+  // target city three's time
+  let cityThreeTimeElement = cityThreeElement.querySelector(".time");
+  // time for third city
+  let cityThreeTime = moment().tz("Europe/Gibraltar");
+  cityThreeDateElement.innerHTML = cityThreeTime.format("MMMM Do, YYYY");
+  cityThreeTimeElement.innerHTML = `${cityThreeTime.format(
+    "h:mm:ss"
+  )} <small id = "ampm">${cityThreeTime.format("A")}</small>`;
+}, 1000);
