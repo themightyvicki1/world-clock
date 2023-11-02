6e54f4ae2a7157158d30e4b7c40737f60a4ed8f3
themightyvicki1 <victoria.greer861@gmail.com>
2023-11-02 12:35:19 -0700

    updated js to change display to only one selected from dropdown

--- src/script.js
+++ src/script.js
@@ -1,14 +1,23 @@
 function selectedDropdown(event) {
   // don't alert the default without value, checking value selected against only 3 value options
   if (event.target.value.length > 0) {
-    let timeZone = moment()
-      .tz(event.target.value)
-      .format("dddd, MMMM D, YYYY H:mm A");
+    let cityTimeZone = event.target.value;
+    let nameOfCity = cityTimeZone.replace("_", " ").split("/")[1];
+    let cityTime = moment().tz(cityTimeZone);
 
-    alert(`It is ${timeZone} in ${event.target.value}`);
+    //alert(`It is ${timeZone} in ${event.target.value}`);
+
+    let selectedElement = document.querySelector("#cities");
+    selectedElement.innerHTML = `<div class="city">
+          <div class = "eachCity">
+            <h2>${nameOfCity}</h2>
+            <div class="date">${cityTime.format("MMMM Do, YYYY")}</div>
+          </div>
+          <div class="time">${cityTime.format(
+            "h:mm:ss"
+          )} <small id = "ampm">${cityTime.format("A")}</small></div>`;
   }
 }
-
 // select the select drop down
 let nameSelect = document.querySelector("#selector");
 nameSelect.addEventListener("change", selectedDropdown);
